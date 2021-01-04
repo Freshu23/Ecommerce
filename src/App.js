284dc3bf07f3ccaedf9b31bce93e29523f323a93
Michał <freshu321@gmail.com>
2021-01-04 13:50:28 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>hats</h1>
+  </div>
+) 
+
 function App() {
   return (
-    <Homepage></Homepage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+      </Switch>
+      
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
